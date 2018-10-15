73c36d5b5d92a062a9ab214496b01356475213f7
ravestudio <ravestudio@mail.ru>
2018-10-15 12:32:13 +0600

    add url provider

--- ClientApp/src/app/post.service.ts
+++ ClientApp/src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -14,14 +14,14 @@ export class PostService {
 
   /** GET posts from the server */
   getPosts (): Observable<Post[]> {
-    return this.http.get<Post[]>(this.postsUrl)
+    return this.http.get<Post[]>(this.baseUrl+this.postsUrl)
   }
 
   /** GET post by permalink. Will 404 if id not found */
   getPost(permalink: string): Observable<Post> {
-    const url = `${this.postsUrl}/${permalink}`;
+    const url = `${this.baseUrl}${this.postsUrl}/${permalink}`;
     return this.http.get<Post>(url);
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 }
